747356957923b60a352495e4e18a7d3f3acb6eb7
PanierAvide <panieravide@riseup.net>
2019-09-08 11:37:51 +0200

    Generate JSON file to allow presets translation

--- fix_i18n_files.js
+++ fix_i18n_files.js
@@ -4,53 +4,55 @@
 
 const fs = require('fs');
 
-const I18N_DIR = "./src/config/locales";
+const I18N_DIRS = [ "./src/config/locales/ui", "./src/config/locales/presets" ];
 const JSON_RGX = /^[A-Za-z0-9_\-]+\.json$/;
 
 //Read translation files
-fs.readdirSync(I18N_DIR).forEach((file) => {
-	if(JSON_RGX.test(file)) {
-		const lng = file.substring(0, file.length - 5).replace("_", "-");
-		try {
-			//Read file
-			const lngData = JSON.parse(fs.readFileSync(I18N_DIR+"/"+file, 'utf8'));
+I18N_DIRS.forEach(i18n_dir => {
+	fs.readdirSync(i18n_dir).forEach((file) => {
+		if(JSON_RGX.test(file)) {
+			const lng = file.substring(0, file.length - 5).replace("_", "-");
+			try {
+				//Read file
+				const lngData = JSON.parse(fs.readFileSync(i18n_dir+"/"+file, 'utf8'));
 
-			//If not already fixed
-			if(lngData[lng] === undefined) {
-				//Check if en available
-				if(lngData["en"] !== undefined) {
-					//Edit object, put en into lng locale
-					const outData = {};
-					outData[lng] = lngData.en;
+				//If not already fixed
+				if(lngData[lng] === undefined) {
+					//Check if en available
+					if(lngData["en"] !== undefined) {
+						//Edit object, put en into lng locale
+						const outData = {};
+						outData[lng] = lngData.en;
 
-					//Overwrite file
-					fs.writeFile(I18N_DIR+"/"+file, JSON.stringify(outData, null, 2), function(err) {
-						if(err) {
-							throw new Error(err);
-						}
-						else {
-							console.log("[INFO] Translation file "+file+" updated");
-						}
-					});
+						//Overwrite file
+						fs.writeFile(i18n_dir+"/"+file, JSON.stringify(outData, null, 2), function(err) {
+							if(err) {
+								throw new Error(err);
+							}
+							else {
+								console.log("[INFO] Translation file "+i18n_dir+"/"+file+" updated");
+							}
+						});
+					}
+					else {
+						throw new Error("Unknown translation locale: "+i18n_dir+"/"+file);
+					}
 				}
 				else {
-					throw new Error("Unknown translation locale: "+file);
+					console.log("[INFO] Translation file "+i18n_dir+"/"+file+" already OK");
 				}
 			}
-			else {
-				console.log("[INFO] Translation file "+file+" already OK");
+			catch(e) {
+				if(e instanceof SyntaxError) {
+					throw new Error("Invalid translation file: "+i18n_dir+"/"+file+" ("+e.message+")");
+				}
+				else {
+					throw e;
+				}
 			}
 		}
-		catch(e) {
-			if(e instanceof SyntaxError) {
-				throw new Error("Invalid translation file: "+file+" ("+e.message+")");
-			}
-			else {
-				throw e;
-			}
+		else {
+			console.log("[INFO] Ignored file "+i18n_dir+"/"+file);
 		}
-	}
-	else {
-		console.log("[INFO] Ignored file "+file);
-	}
+	});
 });
